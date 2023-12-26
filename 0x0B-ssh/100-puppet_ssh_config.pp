599b6a2ce0ffafc01e20b1d62e98318aad7d5e7d
Bamiwo Adebayo <bammietop@gmail.com>
2023-12-26 17:02:35 +0100

    ssh 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,10 +4,12 @@ file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',  # Adjust the path if needed, depending on your system
   line   => 'PasswordAuthentication no',
   match  => '^#?PasswordAuthentication',
+  replace => true,
 }
 
 file_line { 'Declare identity file':
   path   => '/home/your_username/.ssh/config',  # Replace with the correct path to your SSH config file
   line   => 'IdentityFile ~/.ssh/school',
   match  => '^#?IdentityFile',
+  replace => true,
 }
