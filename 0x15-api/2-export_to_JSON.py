72bb14a102d80d4ad2cc1a285a545700a16674cb
Bamiwo Adebayo <bammietop@gmail.com>
2024-01-29 20:54:42 +0100

    API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,9 +4,9 @@ A Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
 
+import json
 import requests
 import sys
-import json
 
 
 def get_todos(employee_id):
